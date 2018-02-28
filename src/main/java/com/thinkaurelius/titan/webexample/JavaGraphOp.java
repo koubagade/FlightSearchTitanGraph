668e883ab6300e61d48a8ebbf6b0e60a656a0bc4
Supriya Bagade <36106773+SupriyaBagade@users.noreply.github.com>
2018-02-28 13:11:32 +0530

    Update JavaGraphOp.java

--- src/main/java/com/thinkaurelius/titan/webexample/JavaGraphOp.java
+++ src/main/java/com/thinkaurelius/titan/webexample/JavaGraphOp.java
@@ -20,14 +20,54 @@ public class JavaGraphOp {
         this.g = gf.getGraph();
     }
 
-    public String listVertices() {
-        List<String> gods = new ArrayList<String>();
+    public List<JSONObject> listVertices() {
+        List<JSONObject> airports = new ArrayList<JSONObject>();
         Iterator<Vertex> itty = g.vertices();
         Vertex v;
+        JSONObject jsonObject;
+
         while (itty.hasNext()) {
             v = itty.next();
-            gods.add((String) v.property("name").value());
+
+            jsonObject = new JSONObject();
+
+            jsonObject.put("id",v.property("id").value().toString());
+            jsonObject.put("name",v.property("name").value().toString());
+            jsonObject.put("city",v.property("city").value().toString());
+            jsonObject.put("state",v.property("state").value().toString());
+            jsonObject.put("country",v.property("country").value().toString());
+            jsonObject.put("place",v.property("place").value().toString());
+
+            airports.add(jsonObject);
+
         }
-        return gods.toString();
+        return airports;
+    }
+
+    public List<JSONObject> listEdges() {
+        List<JSONObject> flights = new ArrayList<JSONObject>();
+        Iterator<Edge> itty1 = g.edges();
+        Edge e;
+
+        JSONObject jsonObject;
+
+        while (itty1.hasNext()) {
+            e = itty1.next();
+
+            jsonObject = new JSONObject();
+
+            jsonObject.put("fid",e.property("fid").value());
+            jsonObject.put("fname",e.property("fname").value());
+            jsonObject.put("arrTime",e.property("arrTime").value());
+            jsonObject.put("depTime",e.property("depTime").value());
+            jsonObject.put("arrDate",e.property("arrDate").value());
+            jsonObject.put("depDate",e.property("depDate").value());
+            jsonObject.put("company",e.property("company").value());
+            jsonObject.put("source",e.property("source").value());
+            jsonObject.put("destination",e.property("destination").value());
+
+            flights.add(jsonObject);
+        }
+        return flights;
     }
 }
